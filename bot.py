519b88847e621f7e6c01ce65deaa8b4d32e66493
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-03 17:01:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,12 @@ from pyrogram import Client
 
 from data import whispers
 
-app = Client(
-     "Whisper-Bot",
-    api_id= 11523090,
-    api_hash="a27541e538e3e1fb05e90297d9140330",
-    bot_token="5410459821:AAFppkpzMGph6MKlIiNF5RJr6DGXdR7Ryt0",
-    plugins = dict(
+plugins = dict(
     root="plugins",
     include=[
         "inline",
-        "private"])
+        "private"
+    ]
 )
 
 # logging.basicConfig(level=logging.DEBUG)
