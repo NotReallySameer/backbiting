5a87c79abd246801c706be7266b1b49d30ce7465
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-03 17:18:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ from data import whispers
 
 api_id = 11144788
 api_hash = "fc82681ae36d60a860633f42252a54d8"
-
-app = Client("5695443548:AAG0I0wombUTOO9FeOh8hpWYKv4WR3Xg2dg", api_id=api_id, api_hash=api_hash)
+bot_token = "5695443548:AAG0I0wombUTOO9FeOh8hpWYKv4WR3Xg2dg"
 
 plugins = dict(
     root="plugins",
@@ -20,7 +19,7 @@ plugins = dict(
 
 # logging.basicConfig(level=logging.DEBUG)
 print('>>> BOT STARTED')
-Client("ezWhisperBot", plugins=plugins).run()
+Client("ezWhisperBot", api_id=api_id, api_hash=api_hash, bot_token=bot_token, plugins=plugins).run()
 with open('data.json', 'w') as f:
     json.dump(whispers, f)
 print('\n>>> BOT STOPPED')
