0135017b84ae2394624fe0ecf155da560b4a1bcb
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-03 14:39:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ from data import whispers
 
 app = Client(
      "Whisper-Bot",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
+    api_id,
+    api_hash,
+    bot_token,
     plugins = dict(
     root="plugins",
     include=[
