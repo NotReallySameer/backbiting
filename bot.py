df4a194d53b63e8afb0f8a58cec98c70e7ec000b
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-03 17:12:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,11 @@ from pyrogram import Client
 
 from data import whispers
 
+api_id = 11144788
+api_hash = "fc82681ae36d60a860633f42252a54d8"
+
+app = Client("5695443548:AAG0I0wombUTOO9FeOh8hpWYKv4WR3Xg2dg", api_id=api_id, api_hash=api_hash)
+
 plugins = dict(
     root="plugins",
     include=[
